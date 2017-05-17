ea15a213c035fa22da68494a8aac3f1555c96bf6
Celoka <eloka.chima@gmail.com>
2017-05-17 12:50:36 +0100

    A solution to find minimum and maximum value in an array

--- app/myfunction.js
+++ app/myfunction.js
@@ -1,30 +1,21 @@
 module.exports =  {
-aritGeo: (arr) => {
+    findMinMax: (arr) => {
+        let minMaxArray = [];
+        let min = arr[0]
+        let max = arr[0]
+        for(let i = 1; i <arr.length;++i){
+            if (min<arr[i]){
+                min =arr[i];
+            }
+            if (max>arr[i]){
+                max = arr[i];
+            }
+        }
+        if(max===min){
+            minMaxArray.push(max);
+        }
+        else minMaxArray.push(max,min);
+        return minMaxArray;
+    }
     
-    let arrayType = -1;
-    if (arr.length <= 0) 
-    return 0;
-    let a = arr[1], r = a/arr[0], i;
-    for (i = 2; i < arr.length; ++i) {
-        if ((a *= r) == arr[i]){
-            arrayType = "Geometric";
-        }
-        else{
-            arrayType = -1;
-        }
-    }
-    if (arrayType == "Geometric")
-    return arrayType;
-    a = arr[1], d = a - arr[0], i;
-    for (i = 2; i < arr.length; ++i) {
-        if ((a += d) == arr[i]){
-            arrayType = "Arithmetic";
-        }
-        else {
-            arrayType = -1;
-        }
-    }
-    return arrayType;
-
   }
-}
--- spec/spec.js
+++ spec/spec.js
@@ -1,61 +1,29 @@
 'use strict';
 const a = require('../app/myfunction.js');
+describe('Min-Max Numbers in a List: ', function () {
 
-  describe("Determine the sequence of an array of numbers: ", function() {
+    describe('Return the min and max number in the list in a new list follows `[min, max]`', function () {
 
-    describe("Case for an empty array", function() {
+      it('should return [1,4] for [1, 2, 3 , 4]', function () {
+        expect(a.findMinMax([1, 2, 3, 4])).toEqual([1, 4]);
+      });
 
-      it("should return 0 for an empty array", function() {
-        expect(a.aritGeo([])).toEqual(0);
+      it('should return [4, 6] for [6, 4]', function () {
+        expect(a.findMinMax([6, 4])).toEqual([4, 6]);
+      });
+
+      it('should return [2, 78] for [4, 66, 6, 44, 7, 78, 8, 68, 2]', function () {
+        expect(a.findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).toEqual([2, 78]);
       });
 
     });
 
-    describe("Case for an arithmetic sequence", function() {
+    describe('Return the number in the list in a new list follows `[min]` when the number is the min and max number in that list', function () {
 
-      it("should return `Arithmetic` for [2, 4, 6, 8, 10]", function() {
-        expect(a.aritGeo([2, 4, 6, 8, 10])).toEqual('Arithmetic');
-      });
-
-      it("should return `Arithmetic` for [5, 11, 17, 23, 29, 35, 41]", function() {
-        expect(a.aritGeo([5, 11, 17, 23, 29, 35, 41])).toEqual('Arithmetic');
-      });
-
-      it("should return `Arithmetic` for [15, 10, 5, 0, -5, -10]", function() {
-        expect(a.aritGeo([15, 10, 5, 0, -5, -10])).toEqual('Arithmetic');
+      it('should return [4] for [4, 4, 4, 4]', function () {
+        expect(a.findMinMax([4, 4, 4, 4])).toEqual([4]);
       });
 
     });
 
-    describe("Case for a geometric sequence", function() {
-
-      it("should return `Geometric` for [2, 6, 18, 54, 162]", function() {
-        expect(a.aritGeo([2, 6, 18, 54, 162])).toEqual('Geometric');
-      });
-
-      it("should return `Geometric` for [0.5, 3.5, 24.5, 171.5]", function() {
-        expect(a.aritGeo([0.5, 3.5, 24.5, 171.5])).toEqual('Geometric');
-      });
-
-      it("should return `Geometric` for [−128, 64, −32, 16, −8]", function() {
-        expect(a.aritGeo([-128, 64, -32, 16, -8])).toEqual('Geometric');
-      });
-
-    });
-
-    describe("Case for neither arithmetic nor geometric sequence", function() {
-
-      it("should return -1 for [1, 2, 3, 5, 8]", function() {
-        expect(a.aritGeo([1, 2, 3, 5, 8])).toEqual(-1);
-      });
-
-      it("should return -1 for [1, 3, 6, 10, 15]", function() {
-        expect(a.aritGeo([1, 3, 6, 10, 15])).toEqual(-1);
-      });
-
-      it("should return -1 for [1, 8, 27, 64, 125]", function() {
-        expect(a.aritGeo([1, 8, 27, 64, 125])).toEqual(-1);
-      });
-      
-    });
   });
